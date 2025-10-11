49c7265696c91f4cfce93bf92ecb979d048ef237
HoangHiep <hoanghiep1418@gmail.com>
2025-10-11 10:45:54 +0700

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 👈 xuất static site thay cho `next export`
-  images: {
-    unoptimized: true, // cần cho Cloudflare Pages
+  experimental: {
+    runtime: 'edge',
   },
   eslint: {
     ignoreDuringBuilds: true,

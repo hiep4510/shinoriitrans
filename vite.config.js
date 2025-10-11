032fd42f3bcb4b57250a55ea05f89f6a1a78f0d6
HoangHiep <hoanghiep1418@gmail.com>
2025-10-11 10:10:51 +0700

    update visit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  build: { outDir: 'dist' }
+  root: '.',             // đảm bảo root ở thư mục hiện tại
+  build: {
+    outDir: 'dist',      // Cloudflare cần thư mục này
+  },
 })

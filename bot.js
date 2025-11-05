5a195de18ed6416508b88907ab5a92c892d5f6ef
HoangHiep <hoanghiep1418@gmail.com>
2025-11-05 15:15:39 +0700

    fix bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,11 @@ const client = new Client({
 
 client.once('ready', () => {
   console.log(`✅ Bot đã đăng nhập với tên: ${client.user.tag}`);
+  const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Bot is running!"));
+app.listen(3000);
+
 });
 
 client.login(process.env.TOKEN);
